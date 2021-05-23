dc39fd02c557216d9f038ad3cfc6a468ea3e65f2
vincentdurufle <vincent.durufle@hotmail.fr>
2021-05-23 17:57:24 +0200

    rm unnecessary

--- src/Controller/CollaborationController.php
+++ src/Controller/CollaborationController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Collaboration;
-use App\Form\CollaborationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,106 +31,8 @@ class CollaborationController extends AbstractController
         $repo = $this->entityManager->getRepository(Collaboration::class);
         $collaborations = $repo->findBy(['disabled' => false]);
 
-        return $this->render('admin/index_items.html.twig', [
+        return $this->render('collaboration/collaboration_index.html.twig', [
             'collaborations' => $collaborations
         ]);
-    }
-
-    /**
-     * @Route("/admin/collaborations", name="admin_collaborations_index")
-     *
-     * @return Response
-     */
-    public function adminIndex(): Response
-    {
-        $repo = $this->entityManager->getRepository(Collaboration::class);
-        $collaborations = $repo->findAll();
-
-        return $this->render('admin/index_items.html.twig', [
-            'collaborations' => $collaborations
-        ]);
-    }
-
-    /**
-     * @Route("admin/collaboration/add", name="collaboration_add")
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
-    public function add(Request $request): Response
-    {
-        $collaboration = new Collaboration();
-        $form = $this->createForm(CollaborationType::class, $collaboration);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($collaboration);
-            $this->entityManager->flush();
-
-            $this->addFlash('success', 'Collaboration successfully added');
-
-            return $this->redirectToRoute('admin_collaborations_index');
-        }
-
-        return $this->render('collaboration/form.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/admin/collaboration/{slug}/edit", name="collaboration_edit")
-     *
-     * @param Request $request
-     * @param string $slug
-     *
-     * @return Response
-     */
-    public function edit(Request $request, string $slug): Response
-    {
-        $repo = $this->entityManager->getRepository(Collaboration::class);
-
-        if (null === $collaboration = $repo->findOneBy(['slug' => $slug])) {
-            $this->createNotFoundException('Collaboration not found');
-        }
-
-        $form = $this->createForm(CollaborationType::class, $collaboration);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($collaboration);
-            $this->entityManager->flush();
-
-            $this->addFlash('success', 'Collaboration successfully edited');
-
-            return $this->redirectToRoute('admin_collaborations_index');
-        }
-
-        return $this->render('admin/add_item.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/admin/collaboration/{slug}/delete", name="collaboration_delete")
-     *
-     * @param string $slug
-     *
-     * @return Response
-     */
-    public function delete(string $slug): Response
-    {
-        $repo = $this->entityManager->getRepository(Collaboration::class);
-
-        if (null === $collaboration = $repo->findOneBy(['slug' => $slug])) {
-            $this->createNotFoundException('Item not found');
-        }
-
-        $this->entityManager->remove($collaboration);
-        $this->entityManager->flush();
-
-        $this->addFlash('success', 'Collaboration successfully deleted');
-
-        return $this->redirectToRoute('admin_collaborations_index');
     }
 }
